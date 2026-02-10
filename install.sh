64b7b94ee0e52f1e32033151d6567d89958d8de7
John T. Foster <john.foster@utexas.edu>
2026-02-10 09:34:51 -0600

    Fix: backup before removing existing dotfiles

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 script_dir=$(dirname "$(readlink -f "$0")")
 
-rm -rf $HOME/.bash_profile
-rm -rf $HOME/.bashrc
-
 cp $HOME/.bash_profile $HOME/.bash_profile_backup 2>/dev/null || true
 cp $HOME/.bashrc $HOME/.bashrc_backup 2>/dev/null || true
+
+rm -rf $HOME/.bash_profile
+rm -rf $HOME/.bashrc
 
 ln -sf $script_dir/bash_profile $HOME/.bash_profile
 ln -sf $script_dir/bashrc $HOME/.bashrc
